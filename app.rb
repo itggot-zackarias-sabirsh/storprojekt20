c408eb8f53144c345f0ea7d6a640085ed882e343
itggot-zackarias-sabirsh <zackarias.sabirsh@elev.ga.ntig.se>
2020-04-22 21:31:24 +0200

    Postningsprocessen helt klar

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ post('/users/new') do
     if validation.empty?
         if  password == password_confirmation
 
-            password_digest = BCrypt::Password.create(password)
+            password_digest = new_password(password)
             p password_digest
             result = new_user(username, password_digest)
             p result
@@ -54,10 +54,10 @@ post('/users/login') do
     
     if validation.any?
 
-        password_digest = password(username)
+        password_digest = get_password_by_username(username)
         p password_digest
 
-        if BCrypt::Password.new(password_digest) == password
+        if validate_password(password_digest) == password
             user_id = login(username)
             p user_id
             session[:user_id] = user_id
@@ -73,8 +73,8 @@ post('/users/login') do
 end
 
 before do
-    puts MIME::Types.type_for('css')
-    session[:user_id] = 3
+    MIME::Types.type_for('css')
+    # session[:user_id] = 3
     path = request.path_info
     blacklist = ['/', '/users/login', '/users/new']
     redirect = true
@@ -84,25 +84,54 @@ before do
             redirect = false
         end
     end
-
+    
     if session[:user_id].nil? and redirect
         redirect('/')
     end
+
+    # all_post_id.each do |post_id|
+    #     post_tags = get_tags_by_post_id(post_id)
+    # end
 end
 
 get('/users/register_confirmation') do
     slim(:register_confirmation)
 end
 
-get('/post/create') do
+get('/create_post') do
     slim(:"post/create")
 end
 
-get('/home') do
- slim(:home, locals:{current_user_id: session[:user_id], current_username: get_username_by_id(session[:user_id])})
+post('/post/create') do
+    post_text = params["post_text"]
+    tag_name = params["tag_name"].split" "
+    p tag_name
+
+    
+    post_user = (session[:user_id])
+    new_post(post_text, post_user)
+    post_id = get_post_id()
+    p post_id
+    
+    
+    tag_name.each do |tag_each| 
+        p tag_each
+        new_tag(tag_each)    
+        tag_id = get_tag_id_by_name(tag_each) 
+        post_tags_join(post_id, tag_id)
+    end
+    
+    
+    redirect('/home')
 end
 
-get ('/error') do
+get('/home') do
+    slim(:home, locals:{current_user_id: session[:user_id], 
+        current_username: get_username_by_id(session[:user_id]), 
+        all_post_info: get_all_post_info()})      
+end
+
+get('/error') do
     slim(:error)
 end
 
--- model.rb
+++ model.rb
@@ -29,7 +29,64 @@ def login(username)
     return result
 end
 
-def password(username)
+def new_password(password)
+    result = BCrypt::Password.create(password)
+    return result
+end
+
+def validate_password(password_digest)
+    result = BCrypt::Password.new(password_digest)
+    return result
+end
+
+def get_password_by_username(username)
     result = db.execute("SELECT password_digest FROM users WHERE username = (?)", username).first['password_digest']
     return result
 end
+
+def new_tag(tag_each)
+    tag_validation = db.execute("SELECT tag_name FROM tags WHERE tag_name = (?)", tag_each)
+
+    if tag_validation.empty?
+        result = db.execute("INSERT INTO tags (tag_name) VALUES (?)", [tag_each])
+        return result
+    end
+end
+
+def get_tag_id_by_name(tag_each)
+    result = db.execute("SELECT tag_id FROM tags WHERE tag_name = (?)", tag_each).first['tag_id']
+    return result
+end
+
+def get_tags_by_post_id(id)
+    result = db.execute("SELECT post_tags_join.post_id, tags.tag_name FROM post_tags_join INNER JOIN tags ON post_tags_join.post_tag = tags.tag_id WHERE post_id =(?)", id)
+    return result
+end
+
+def new_post(post_text, post_user)
+    result = db.execute("INSERT INTO posts (post_text, post_user) VALUES (?, ?)", [post_text, post_user])
+end
+
+def get_post_id()
+    result = db.execute("SELECT MAX(post_id) from posts").first['MAX(post_id)']
+    return result
+end
+
+def post_tags_join(latest_post_id, tag_id)
+    result = db.execute("INSERT INTO post_tags_join (post_id, post_tag) VALUES (?, ?)", [latest_post_id, tag_id])
+end
+
+def get_all_post_info()
+    result = db.execute("SELECT * from posts")
+    return result
+end
+
+def get_post_info_from_id(post_id)
+    result = db.execute("SELECT * from posts WHERE post_id = (?)", post_id).first['post_id']
+    return result
+end
+
+def all_post_id()
+    all_post_id = db.execute("SELECT post_id FROM posts").first['post_id']
+end
+
